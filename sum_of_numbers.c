c3b97f205f94bdfc41d900c16d42ddb017876633
Harish21052004 <126686602+Harish21052004@users.noreply.github.com>
2023-07-20 20:53:18 +0530

    Create sum_of_numbers.c

---
+++ sum_of_numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    printf("Enter the numbers: \n");
+    scanf("%d %d",&num1,&num2);
+    printf("Sum is %d.\n",num1+num2);
+    return 0;
+}
